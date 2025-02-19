e09c2a1771898ba53479c332d741eef989ec1cc9
suhas t r <36911828+suhastr@users.noreply.github.com>
2025-02-18 22:21:16 -0500

    Update settings.py

--- backend/ai_blog_app/ai_blog_app/settings.py
+++ backend/ai_blog_app/ai_blog_app/settings.py
@@ -77,24 +77,12 @@ WSGI_APPLICATION = 'ai_blog_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydbdemo',  # Your Cloud SQL database name
-        'USER': 'susz',  # Your Cloud SQL username
-        'PASSWORD': '4(*Jn$eJT``zXDMz',  # Your Cloud SQL password
-        'HOST': '34.55.216.115',  # Use the Public IP from GCP
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
